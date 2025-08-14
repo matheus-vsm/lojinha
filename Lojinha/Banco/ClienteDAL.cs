a1c36fe3feb1bee650166a9e432bd4fee080c2f1
matheus-vsm <scompinha@hotmail.com>
2025-08-13 23:39:01 -0300

    inicio da criação dos DAL

---
+++ Lojinha/Banco/ClienteDAL.cs
@@ -0,0 +1,47 @@
+﻿using Lojinha.Modelos;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lojinha.Banco
+{
+    public class ClienteDAL
+    {
+        private readonly LojinhaContext context; //para conseguir ser acessado pelas classes que herdam dela
+
+        public ClienteDAL(LojinhaContext context)
+        {
+            this.context = new LojinhaContext(); //inicializa o contexto do banco de dados
+        }
+
+        public IEnumerable<Cliente> Listar()
+        {
+            return context.Clientes.ToList();
+        }
+
+        public void Adicionar(Cliente cliente)
+        {
+            context.Clientes.Add(cliente);
+            context.SaveChanges();
+        }
+
+        public void Deletar(int id)
+        {
+            var cliente = context.Clientes.Find(id);
+            if (cliente != null)
+            {
+                context.Clientes.Remove(cliente);
+                context.SaveChanges();
+            }
+        }
+
+        public void Atualizar(Cliente cliente)
+        {
+            context.Clientes.Update(cliente);
+            context.SaveChanges();
+        }
+    }
+}
--- Lojinha/Program.cs
+++ Lojinha/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Lojinha.Banco;
+
+var context = new LojinhaContext(); // Inicializa o contexto do banco de dados
+var clienteDAL = new ClienteDAL(context);
+var listaClientes = clienteDAL.Listar();
+foreach (var cliente in listaClientes)
+{
+    Console.WriteLine($"ID: {cliente.Id} - Nome: {cliente.Nome} - Email: {cliente.Email}");
+}
