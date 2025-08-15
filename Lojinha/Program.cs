815f0771c7cb8c615d379b175b86fdf869c9a1a7
matheus-vsm <scompinha@hotmail.com>
2025-08-15 00:59:46 -0300

    a

--- Lojinha/Program.cs
+++ Lojinha/Program.cs
@@ -1,7 +1,11 @@
 ﻿using Lojinha.Banco;
+using Lojinha.Modelos;
 
 var context = new LojinhaContext(); // Inicializa o contexto do banco de dados
 var clienteDAL = new ClienteDAL(context);
+
+clienteDAL.Deletar(6);
+
 var listaClientes = clienteDAL.Listar();
 foreach (var cliente in listaClientes)
 {
