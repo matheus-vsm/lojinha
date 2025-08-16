a712189745923ed68a2486d66454017db87216f5
matheus-vsm <scompinha@hotmail.com>
2025-08-15 23:18:09 -0300

    l

--- Lojinha/Banco/DAL.cs
+++ Lojinha/Banco/DAL.cs
@@ -3,16 +3,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Lojinha.Banco
 {
-    public class DAL<T> where T : class
+    public abstract class DAL<T> where T : class
     {
-        private readonly LojinhaContext context;
+        protected readonly LojinhaContext context;
 
         public DAL(LojinhaContext context)
         {
             this.context = context;
+        }
+
+        public IEnumerable<T> Listar()
+        {
+            return context.Set<T>().ToList();
+        }
+        public T? BuscarPorId(object id) // object é usado porque o EF permite vários tipos de chave. (int, string, etc.)
+        {
+            return context.Set<T>().Find(id);
+        }
+        public T? BuscarPor(Func<T, bool> condicao) // <Tipo, retorno>
+        {
+            return context.Set<T>().FirstOrDefault(condicao);
+        }
+
+        public void Adicionar(T objeto)
+        {
+            context.Set<T>().Add(objeto);
+            context.SaveChanges();
+        }
+        public void Deletar(T objeto)
+        {
+            context.Set<T>().Remove(objeto);
+            context.SaveChanges();
+
+        }
+        public void Atualizar(T entity)
+        {
+            context.Set<T>().Update(entity);
+            context.SaveChanges();
         }
     }
 }
